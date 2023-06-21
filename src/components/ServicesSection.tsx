88805abeba8b7870402ab2fca549ba6c3fdd5aca
Matias Seitour <matias.seitour01@gmail.com>
2023-06-21 17:17:14 -0300

    feat(services) add services

---
+++ src/components/ServicesSection.tsx
@@ -0,0 +1,9 @@
+export const ServicesSection = (): JSX.Element => {
+  return (
+    <section className="h-screen w-screen bg-red-200 pt-10">
+      <div className="h-auto w-full">
+        <h2 className="text-center text-[3rem]">Plataformas Digitales</h2>
+      </div>
+    </section>
+  );
+};
