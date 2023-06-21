770ef4d7082499dc01a2ed2a20ecc24e32376b5b
Matias Seitour <matias.seitour01@gmail.com>
2023-06-21 17:17:00 -0300

    feat(banner) add button link

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -5,7 +5,7 @@ import { useEffect } from "react";
 import SplitType from "split-type";
 
 const paytone_One = Paytone_One({
-  weight: "400",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
@@ -69,6 +69,7 @@ export const Banner = () => {
       >
         ¿Que me están cobrando?
       </h1>
+
       <div className="mb-4">
         <h2 className="home-subtitle mb-10 mt-10 text-2xl tracking-wide text-white">
           Descubre que impuestos te cobran y cúal es el valor final para evitar
@@ -77,6 +78,21 @@ export const Banner = () => {
       </div>
 
       <SliderHome />
+
+      <button
+        className="button-calculator flex flex-row items-center rounded-md border border-white bg-white p-2 transition-all hover:bg-[#000] hover:text-white"
+        type="button"
+      >
+        <p className="mr-2">Calculadora Dólar</p>
+        <svg
+          className="svg-calculator"
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 512 512"
+        >
+          <path d="M320 0c-17.7 0-32 14.3-32 32s14.3 32 32 32h82.7L201.4 265.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L448 109.3V192c0 17.7 14.3 32 32 32s32-14.3 32-32V32c0-17.7-14.3-32-32-32H320zM80 32C35.8 32 0 67.8 0 112V432c0 44.2 35.8 80 80 80H400c44.2 0 80-35.8 80-80V320c0-17.7-14.3-32-32-32s-32 14.3-32 32V432c0 8.8-7.2 16-16 16H80c-8.8 0-16-7.2-16-16V112c0-8.8 7.2-16 16-16H192c17.7 0 32-14.3 32-32s-14.3-32-32-32H80z" />
+        </svg>
+      </button>
     </div>
   );
 };
