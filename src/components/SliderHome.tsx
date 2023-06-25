f2709517091ab77f1a7992e7b78c2ceb6ae1c9d8
Matias Seitour <matias.seitour01@gmail.com>
2023-06-24 23:48:58 -0300

    fix(slider) add max-width

--- src/components/SliderHome.tsx
+++ src/components/SliderHome.tsx
@@ -2,10 +2,8 @@ import Image from "next/image";
 import plataformas from "../assets/plataformas.json";
 
 export const SliderHome = () => {
-  console.log(plataformas);
-
   return (
-    <div className="slider-container relative mb-20 grid h-auto w-[50%] place-items-center overflow-hidden">
+    <div className="slider-container relative mb-20 grid h-auto w-[50%] max-w-[684px] place-items-center overflow-hidden">
       <ul className="slider-track animate-sliderScroll">
         {plataformas.plataformas.map((plataforma) => (
           <li key={plataforma.id} className="h-[80px] w-[100px] p-4">
