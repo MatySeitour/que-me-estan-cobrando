5146ff6c5c59718d47d430deb3786222be16b62d
Matias Seitour <matias.seitour01@gmail.com>
2023-06-24 23:50:16 -0300

    feat(services) add services section

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -1,8 +1,36 @@
+import TitleServices from "./TitleServices";
+import plataformas from "../assets/plataformas.json";
+import { Service } from "@/types";
+import { Services } from "./Services";
+
 export const ServicesSection = (): JSX.Element => {
+  interface ServiceProps {
+    Service: Service;
+  }
   return (
-    <section className="h-screen w-screen bg-red-200 pt-10">
-      <div className="h-auto w-full">
-        <h2 className="text-center text-[3rem]">Plataformas Digitales</h2>
+    <section className="h-auto w-full pt-10">
+      <div className="mb-10 h-auto w-full">
+        <TitleServices />
+      </div>
+      <div className="flex min-h-screen w-full flex-col items-center justify-center px-4 py-4">
+        {plataformas.plataformas.map((plataform) => (
+          <div key={plataform.id} className="mb-10 flex h-96 w-3/4">
+            <Services
+              id={plataform.id}
+              nombre={plataform.nombre}
+              imagen={plataform.imagen}
+              planes={plataform.planes}
+              impuesto_final={plataform.impuesto_final}
+              impuesto_porcentaje={plataform.impuesto_porcentaje}
+              select_price={plataform.select_price}
+            />
+            <div className="h-full w-full bg-white/50">
+              <div className="flex h-auto w-full justify-center pt-4">
+                <h4 className="bg-gradient__effect text-3xl">Precios</h4>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
