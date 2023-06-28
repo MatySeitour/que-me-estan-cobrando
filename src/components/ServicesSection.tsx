6fec37e4533afa257d36ac493940d26465d37da8
Matias Seitour <matias.seitour01@gmail.com>
2023-06-28 19:34:47 -0300

    feat(prices) add prices ^&^& services section

--- src/components/ServicesSection.tsx
+++ src/components/ServicesSection.tsx
@@ -14,7 +14,7 @@ export const ServicesSection = (): JSX.Element => {
       </div>
       <div className="flex min-h-screen w-full flex-col items-center justify-center px-4 py-4">
         {plataformas.plataformas.map((plataform) => (
-          <div key={plataform.id} className="mb-10 flex h-96 w-3/4">
+          <div key={plataform.id} className="mb-10 flex h-96 w-full">
             <Services
               id={plataform.id}
               nombre={plataform.nombre}
@@ -24,11 +24,6 @@ export const ServicesSection = (): JSX.Element => {
               impuesto_porcentaje={plataform.impuesto_porcentaje}
               select_price={plataform.select_price}
             />
-            <div className="h-full w-full bg-white/50">
-              <div className="flex h-auto w-full justify-center pt-4">
-                <h4 className="bg-gradient__effect text-3xl">Precios</h4>
-              </div>
-            </div>
           </div>
         ))}
       </div>
