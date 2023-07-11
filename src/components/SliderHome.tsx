a7745e8d4ba4700a4356d9d527ac4417e883ddad
Matias Seitour <matias.seitour01@gmail.com>
2023-07-11 19:34:29 -0300

    fix(slider) change size images

--- src/components/SliderHome.tsx
+++ src/components/SliderHome.tsx
@@ -6,22 +6,28 @@ export const SliderHome = () => {
     <div className="slider-container relative mb-20 grid h-auto w-[50%] max-w-[684px] place-items-center overflow-hidden">
       <ul className="slider-track animate-sliderScroll">
         {plataformas.plataformas.map((plataforma) => (
-          <li key={plataforma.id} className="h-[80px] w-[100px] p-4">
+          <li
+            key={plataforma.id}
+            className="flex h-[100px] w-[100px] items-center justify-center p-4"
+          >
             <Image
               alt={plataforma.nombre}
-              width={100}
-              height={80}
+              width={2000}
+              height={2000}
               className="object-cover"
               src={plataforma.imagen}
             />
           </li>
         ))}
         {plataformas.plataformas.map((plataforma) => (
-          <li key={plataforma.id} className="h-[80px] w-[100px] p-4">
+          <li
+            key={plataforma.id}
+            className="flex h-[100px] w-[100px] items-center justify-center p-4"
+          >
             <Image
               alt={plataforma.nombre}
-              width={100}
-              height={80}
+              width={2000}
+              height={2000}
               className="object-cover"
               src={plataforma.imagen}
             />
