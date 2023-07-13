ee323c1e4ed39e75cc4efad6b6d2f19c867e3e23
Matias Seitour <matias.seitour01@gmail.com>
2023-07-13 19:33:44 -0300

    feat(plan) add animations

--- src/components/Plan.tsx
+++ src/components/Plan.tsx
@@ -1,7 +1,9 @@
-import { useState, useRef } from "react";
+import { useState, useRef, useEffect } from "react";
 import { FaAngleDown } from "react-icons/fa6";
 import { inter } from "@/utils/fonts";
 import plataformasData from "../assets/plataformas.json";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
 
 export const Plan = ({ plans }: { plans: any }): JSX.Element => {
   const [planSelect, setPlanSelect] = useState<number | null>(null);
@@ -14,7 +16,6 @@ export const Plan = ({ plans }: { plans: any }): JSX.Element => {
   const element1: any = useRef(null);
 
   const toggleTax = (numberPlan: number) => {
-    console.log(element1.current?.scrollHeight);
     if (numberPlan != planSelect) {
       setPlanSelect(numberPlan);
       let la = element1.current?.scrollHeight.toString() + "px";
@@ -24,27 +25,63 @@ export const Plan = ({ plans }: { plans: any }): JSX.Element => {
       element1.current.style.height = "4rem";
     }
   };
+
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    const titleCard = gsap.utils.toArray(`#plans`);
+
+    const tl = gsap.timeline({ paused: true, reversed: true });
+
+    tl.play();
+
+    titleCard.forEach((plan: any) => {
+      tl.fromTo(
+        plan,
+        {
+          yPercent: -20,
+          opacity: 0,
+          duration: 0.2,
+        },
+        {
+          yPercent: 0,
+          opacity: 1,
+          duration: 0.2,
+        }
+      );
+    });
+  }, []);
   return (
     <li
+      id={`plans`}
       ref={element1}
       onClick={() => toggleTax(plans.id)}
       className={
         plans.id == planSelect
-          ? ` border-t border-white/30 text-white transition-[height]`
-          : `h-16 overflow-hidden border-t border-white/30 text-white transition-[height]`
+          ? ` border-t border-white/30 transition-[height]`
+          : `h-16 border-t border-white/30 transition-[height]`
       }
       key={plans.id}
     >
       <div className="flex items-center justify-between p-4">
-        <div className="bg-gradient__effect flex font-normal text-transparent">
-          <p className="mr-2">{plans.plan}</p>
-          <b className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal">
-            ${`${plans.precio}`}
-          </b>
-          <span>/</span>
-          <b className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal">
-            ${`${finalPrice}`}
-          </b>
+        <div className="flex font-normal">
+          <p className="bg-gradient__effect mr-2">{plans.plan}</p>
+          <div className="group relative">
+            <p className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal text-transparent">
+              ${`${plans.precio}`}
+            </p>
+            <span className="service-message invisible absolute -top-14 left-[50%] -translate-x-1/2 whitespace-nowrap rounded-md border border-white/20 bg-black p-2 text-sm text-red-300 opacity-0 transition-all before:absolute before:-bottom-2 before:left-1/2 before:h-4 before:w-4 before:-translate-x-1/2 before:rotate-45 before:border-b before:border-r before:border-white/20 before:bg-black group-hover:visible group-hover:opacity-100">
+              <p className="text-white">Precio Inicial</p>
+            </span>
+          </div>
+          <span className="bg-gradient__effect">/</span>
+          <div className="group relative">
+            <p className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal text-transparent">
+              ${`${finalPrice}`}
+            </p>
+            <span className="service-message invisible absolute -top-14 left-[50%] -translate-x-1/2 whitespace-nowrap rounded-md border border-white/20 bg-black p-2 text-sm text-red-300 opacity-0 transition-all before:absolute before:-bottom-2 before:left-1/2 before:h-4 before:w-4 before:-translate-x-1/2 before:rotate-45 before:border-b before:border-r before:border-white/20 before:bg-black group-hover:visible group-hover:opacity-100">
+              <p className="text-white">Precio Final</p>
+            </span>
+          </div>
         </div>
         <FaAngleDown
           className={`h-5 w-5 text-white transition-all ${
@@ -58,7 +95,11 @@ export const Plan = ({ plans }: { plans: any }): JSX.Element => {
         }`}
       >
         <ul
-          className={`p-4 font-serif font-normal ${inter.className} flex flex-col font-bold text-white/70`}
+          className={
+            plans.id == planSelect
+              ? `p-4 font-serif font-normal ${inter.className} visible flex flex-col font-bold text-white/70 opacity-100 transition-[opacity]`
+              : `p-4 font-serif font-normal ${inter.className} invisible flex flex-col font-bold text-white/70 opacity-0 transition-[opacity]`
+          }
         >
           {plans.descripcion.map((planDescription: any) => (
             <li key={planDescription.plan_description_id}>
--- src/components/Plans.tsx
+++ src/components/Plans.tsx
@@ -1,18 +1,38 @@
 import { ServiceType } from "@/types";
-import { useRef, useState } from "react";
+import { useRef, useEffect } from "react";
 import { Plan } from "./Plan";
+import { gsap } from "gsap";
 
 export const Plans = ({
   serviceSelected,
 }: {
   serviceSelected: ServiceType;
 }): JSX.Element => {
-  // 154
+  const planTitle = useRef(null);
+
+  useEffect(() => {
+    gsap.fromTo(
+      planTitle.current,
+      {
+        yPercent: -40,
+        opacity: 0,
+        duration: 0.5,
+      },
+      {
+        yPercent: 0,
+        opacity: 1,
+        duration: 0.5,
+      }
+    );
+  }, []);
 
   return (
     <div className="flex h-auto w-full flex-col gap-8">
       <div className="pl-2">
-        <h3 className="bg-gradient__effect text-3xl font-normal text-transparent">
+        <h3
+          ref={planTitle}
+          className="bg-gradient__effect text-3xl font-normal text-transparent"
+        >
           ¿Que planes me ofrecen?
         </h3>
       </div>
