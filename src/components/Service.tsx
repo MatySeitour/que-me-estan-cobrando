f4c0ba7e65790e46dd9b58de8b1e76dd9c0e36da
Matias Seitour <matias.seitour01@gmail.com>
2023-07-18 16:25:39 -0300

    feat(service) image logo size and remove absolute to select service options

--- src/components/Service.tsx
+++ src/components/Service.tsx
@@ -76,45 +76,49 @@ export const Service = ({
     <section className="mb-20 h-auto pt-28">
       <div
         ref={service}
-        className="relative h-full w-full rounded-md border border-white/20 pb-8"
+        className="relative h-full w-full rounded-md border border-white/20"
       >
         <div className="border-effect__top absolute -top-0.5 left-20 h-0.5 w-20 -translate-x-1/2"></div>
         <div className="border-effect__left absolute -left-0.5 top-20 h-20 w-0.5"></div>
         <div className="border-effect__left absolute -right-0.5 top-20 h-20 w-0.5"></div>
         <div className="border-effect__bottom absolute -bottom-0.5 -right-0.5 h-0.5 w-20"></div>
-        <div className="absolute left-1/2 top-2 flex h-auto w-auto -translate-x-1/2 flex-row gap-6 text-white">
-          <div
-            onClick={() => setServiceOptionSelect(1)}
-            className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] before:bg-red-400 ${
-              serviceOptionSelect == 1 && `option-active__1`
-            }`}
-          >
-            <p className="p-2">Precios</p>
-          </div>
-          <div
-            onClick={() => setServiceOptionSelect(2)}
-            className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] before:bg-red-400 ${
-              serviceOptionSelect == 2 && `option-active__2`
-            }`}
-          >
-            <p className="p-2">Impuestos</p>
-          </div>
-          <div
-            onClick={() => setServiceOptionSelect(3)}
-            className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] before:bg-red-400 ${
-              serviceOptionSelect == 3 && `option-active__3`
-            }`}
-          >
-            <p className="p-2">Planes</p>
-          </div>
-          <div className="item-b absolute -left-3 h-10 w-[6.5rem] overflow-hidden rounded-md border border-white/40 transition-all">
-            <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 h-full w-full">
-              <div className="relative flex h-full w-full place-items-center before:absolute before:h-full before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:bottom-4 after:left-[0%] after:-z-20 after:h-[100px] after:w-[140px] after:translate-x-1/3 after:bg-gradient-conic after:from-[#39f2aea5]/30 after:via-[#2D5FFF]/50 after:blur-2xl after:content-['']" />
+        <div className=" flex h-auto w-auto flex-row items-center justify-center px-2 pt-4 text-white">
+          <div className="relative flex flex-row gap-6">
+            <div
+              onClick={() => setServiceOptionSelect(1)}
+              className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] ${
+                serviceOptionSelect == 1 && `option-active__1`
+              }`}
+            >
+              <p className="p-2">Precios</p>
+            </div>
+            <div
+              onClick={() => setServiceOptionSelect(2)}
+              className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] ${
+                serviceOptionSelect == 2 && `option-active__2`
+              }`}
+            >
+              <p className="p-2">Impuestos</p>
+            </div>
+            <div
+              onClick={() => setServiceOptionSelect(3)}
+              className={`relative rounded-md before:absolute before:bottom-0 before:h-[0.1rem] ${
+                serviceOptionSelect == 3 && `option-active__3`
+              }`}
+            >
+              <p className="p-2">Planes</p>
+            </div>
+            <div
+              className={`item-b absolute -left-0 h-10 w-[6.5rem] overflow-hidden rounded-md border border-white/40 transition-all`}
+            >
+              <div className="absolute bottom-0 left-0 right-0 top-0 -z-10 h-full w-full">
+                <div className="relative flex h-full w-full place-items-center before:absolute before:h-full before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:bottom-4 after:left-[0%] after:-z-20 after:h-[100px] after:w-[140px] after:translate-x-1/3 after:bg-gradient-conic after:from-[#39f2aea5]/30 after:via-[#2D5FFF]/50 after:blur-2xl after:content-['']" />
+              </div>
             </div>
           </div>
         </div>
         <div className="flex w-full justify-center md:justify-start">
-          <div className="flex h-40 w-40 items-center justify-center px-4">
+          <div className="flex h-40 w-48 items-center justify-center px-4 sm:w-52">
             <Image
               ref={logoService}
               alt={serviceSelected?.nombre}
