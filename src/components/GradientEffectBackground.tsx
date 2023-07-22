3d52e92b401c2e8591d28952298d85a26ae4974c
Matias Seitour <matias.seitour01@gmail.com>
2023-07-21 21:44:17 -0300

    feat(gradientEffect) change height, width and bg-gradient

--- src/components/GradientEffectBackground.tsx
+++ src/components/GradientEffectBackground.tsx
@@ -1,7 +1,7 @@
 export const GradientEffectBackground = (): JSX.Element => {
   return (
-    <div className="absolute -left-0 bottom-0 -z-10 h-full w-full">
-      <div className="relative flex h-full w-full place-items-center before:absolute before:h-full before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:bottom-4 after:left-[0%] after:-z-20 after:h-[100px] after:w-[140px] after:translate-x-1/3 after:bg-gradient-conic after:from-[#39f2aea5]/30 after:via-[#2D5FFF]/50 after:blur-2xl after:content-['']" />
+    <div className="absolute left-0 top-0 z-0 h-96 w-full">
+      <div className="relative flex h-full w-full place-items-center before:absolute before:left-0 before:top-0 before:h-full before:w-full before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:left-[50%] after:top-0 after:-z-20 after:h-32 after:w-[34rem] after:-translate-x-1/2 after:bg-gradient-conic after:from-[#2D5FFF]/20 after:via-[#39f2aea5]/20 after:blur-2xl after:content-['']" />
     </div>
   );
 };
