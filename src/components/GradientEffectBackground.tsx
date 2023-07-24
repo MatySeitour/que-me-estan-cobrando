4968971b82fcfa68d0675b25caa1ae80742c2773
Matias Seitour <matias.seitour01@gmail.com>
2023-07-23 21:20:22 -0300

    feat(gradientEffect) add classname prop

--- src/components/GradientEffectBackground.tsx
+++ src/components/GradientEffectBackground.tsx
@@ -1,7 +1,15 @@
-export const GradientEffectBackground = (): JSX.Element => {
+export const GradientEffectBackground = ({
+  classname,
+}: {
+  classname?: string;
+}): JSX.Element => {
   return (
     <div className="absolute left-0 top-0 z-0 h-96 w-full">
-      <div className="relative flex h-full w-full place-items-center before:absolute before:left-0 before:top-0 before:h-full before:w-full before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:left-[50%] after:top-0 after:-z-20 after:h-32 after:w-[34rem] after:-translate-x-1/2 after:bg-gradient-conic after:from-[#2D5FFF]/20 after:via-[#39f2aea5]/20 after:blur-2xl after:content-['']" />
+      <div
+        className={`relative flex h-full w-full place-items-center before:absolute before:left-0 before:top-0 before:h-full before:w-full before:rounded-full before:bg-gradient-radial before:from-transparent before:to-[#0B121C10] after:absolute after:left-[50%] after:top-0 after:-z-20 after:h-32 after:w-[34rem] ${
+          classname && classname
+        } after:-translate-x-1/2 after:bg-gradient-conic after:from-[#2D5FFF]/20 after:via-[#39f2aea5]/20 after:blur-2xl after:content-['']`}
+      />
     </div>
   );
 };
