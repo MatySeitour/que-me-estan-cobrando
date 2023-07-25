cdeb416fb2f8a6b545a2f6c812add80dd24aadb7
Matias Seitour <matias.seitour01@gmail.com>
2023-07-25 11:05:43 -0300

    feat(calculatorContainer) add dollar values

--- src/components/CalculatorContainer.tsx
+++ src/components/CalculatorContainer.tsx
@@ -14,11 +14,14 @@ export const CalculatorContainer = ({
   const [badgeCalculator, setBadgeCalculator] = useState<Badge>(Badge.PESOS);
   const [inputCalculator, setInputCalculator] = useState<string>("");
 
+  console.log(dollarCalculator);
+
   const dollarValues = dollarCalculator?.map((dolar: any) => {
-    const values = { nombre: "", compra: null, venta: null };
+    const values = { nombre: "", compra: null, venta: null, variacion: "" };
     values.nombre = dolar.casa.nombre;
     values.compra = dolar.casa.compra.replace(",", ".");
     values.venta = dolar.casa.venta.replace(",", ".");
+    values.variacion = dolar.casa.variacion?.replace(",", ".");
     return values;
   });
 
@@ -35,14 +38,14 @@ export const CalculatorContainer = ({
   };
 
   return (
-    <div className="mx-auto flex h-full flex-col gap-4">
+    <div className="mx-auto flex h-full w-full max-w-4xl flex-col gap-10 p-4">
       <h1
-        className={`home-title bg-clip-text text-5xl ${paytone_One.className}`}
+        className={`calculator-gradient__text bg-clip-text text-5xl ${paytone_One.className} text-center`}
       >
         ¿A cuanto el dólar?
       </h1>
-      <div className="flex h-96 w-full flex-col gap-2 rounded-md border border-white/30 p-2">
-        <div className="relative flex flex-row justify-center gap-6">
+      <div className="flex h-auto w-full flex-col gap-2 rounded-md border border-white/30 p-2">
+        <div className="relative mb-2 flex flex-row justify-center gap-6">
           <div
             onClick={() => setBadgeCalculator(Badge.PESOS)}
             className={`relative rounded-md text-white before:absolute before:bottom-0 before:h-[0.1rem] ${
@@ -50,7 +53,7 @@ export const CalculatorContainer = ({
             }`}
           >
             <p className={`p-2 sm:cursor-pointer ${paytone_One.className}`}>
-              Pesos
+              ARS$
             </p>
           </div>
           <div
@@ -71,10 +74,19 @@ export const CalculatorContainer = ({
             </div>
           </div>
         </div>
+        <div className="mb-2 flex justify-center">
+          <p className="home-title mr-2 bg-clip-text text-lg font-bold">
+            Dólar oficial:{" "}
+          </p>
+
+          <b className="calculator-gradient__text text-lg">
+            ${dollarValues != undefined && Number(dollarValues[0].venta)}
+          </b>
+        </div>
         <div className="w-full">
           <div className="flex w-full justify-end">
             <label
-              className="text-right font-semibold text-white"
+              className="home-title bg-clip-text text-right font-bold"
               htmlFor="calculator"
             >
               {badgeCalculator == Badge.USD
@@ -83,16 +95,21 @@ export const CalculatorContainer = ({
             </label>
           </div>
           <div className="flex h-16 w-full flex-col justify-between rounded-md bg-white/10 p-2 outline-none">
-            <span className="text-right tracking-wide text-white">
+            <span className="home-title bg-clip-text text-right font-bold">
               {badgeCalculator == Badge.PESOS
                 ? `Dólar Oficial: $${
                     dollarValues != undefined
-                      ? Number(inputCalculator) / Number(dollarValues[0].venta)
+                      ? (
+                          Number(inputCalculator) /
+                          Number(dollarValues[0].venta)
+                        ).toFixed(2)
                       : "0"
                   }`
                 : `ARS$: ${
                     dollarValues != undefined &&
-                    Number(inputCalculator) * Number(dollarValues[0].venta)
+                    (
+                      Number(inputCalculator) * Number(dollarValues[0].venta)
+                    ).toFixed(2)
                   }`}
             </span>
             <div className="flex w-full justify-end gap-2">
@@ -103,11 +120,48 @@ export const CalculatorContainer = ({
                 type="text"
                 pattern="[0-9]+"
                 id="calculator"
-                className="h-full w-auto bg-transparent text-right text-white outline-none"
+                className="home-title h-full w-full bg-transparent bg-clip-text text-right font-bold outline-none"
               />
             </div>
           </div>
         </div>
+        <ul className="flex flex-col gap-3 py-2">
+          <li className="flex flex-row justify-between rounded-md bg-white/10 p-2">
+            <p className="home-title bg-clip-text text-right font-bold">
+              TIPO DE CAMBIO
+            </p>
+            <div className="flex justify-center gap-4 text-white">
+              <p className="calculator-gradient__text bg-clip-text font-bold">
+                VENTA
+              </p>
+              <p>/</p>
+              <p className="home-title bg-clip-text text-right font-bold">
+                COMPRA
+              </p>
+            </div>
+          </li>
+          {dollarValues?.map((dolarInfo: any) => (
+            <li
+              key={dolarInfo.nombre}
+              className="flex justify-between rounded-md bg-white/10 p-2"
+            >
+              <div className="flex flex-col gap-4 text-left text-white">
+                <p className="home-title bg-clip-text text-left font-bold">
+                  {dolarInfo.nombre}
+                </p>
+                <p>Variacion: {dolarInfo.variacion}</p>
+              </div>
+              <div className="flex flex-col gap-4 text-right text-white">
+                <p className="calculator-gradient__text bg-clip-text text-xl font-bold">
+                  ${dolarInfo.venta}
+                </p>
+                <p className="home-title bg-clip-text text-right font-bold">
+                  ${dolarInfo.compra}
+                </p>
+              </div>
+            </li>
+          ))}
+        </ul>
       </div>
     </div>
   );
