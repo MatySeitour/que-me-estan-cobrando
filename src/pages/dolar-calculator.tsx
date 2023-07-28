96fe63ce7c9f0a68a262de73e5833ef4c942171a
Matias Seitour <matias.seitour01@gmail.com>
2023-07-28 13:17:15 -0300

    feat(dolar-calculator) add loading data state

--- src/pages/dolar-calculator.tsx
+++ src/pages/dolar-calculator.tsx
@@ -9,33 +9,41 @@ export default function DollarCalculator() {
   const { cotizaciones } = quotesData;
 
   const [dollarCalculator, setDollarCalculator] = useState<any>();
+  const [loadingData, setLoadingData] = useState<boolean>(false);
   useEffect(() => {
     async function getDollar() {
-      const res = await axios.get(
-        "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
-      );
-      const dollarValues = res.data?.map((dolar: any) => {
-        const values = {
-          nombre: "",
-          compra: null,
-          venta: null,
-          variacion: "",
-          descripcion: "",
-        };
-        const descripcionDollar = cotizaciones.filter(
-          (quote) => quote.name == dolar.casa.nombre
+      try {
+        setLoadingData(true);
+        const res = await axios.get(
+          "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
         );
-        console.log(descripcionDollar);
-        values.nombre = dolar.casa.nombre;
-        values.compra = dolar.casa.compra.replace(",", ".");
-        values.venta = dolar.casa.venta.replace(",", ".");
-        values.variacion = dolar.casa.variacion?.replace(",", ".");
-        values.descripcion = !descripcionDollar.length
-          ? ""
-          : descripcionDollar[0].descripcion;
-        return values;
-      });
-      setDollarCalculator(dollarValues);
+        const dollarValues = res.data?.map((dolar: any) => {
+          const values = {
+            nombre: "",
+            compra: null,
+            venta: null,
+            variacion: "",
+            descripcion: "",
+          };
+          const descripcionDollar = cotizaciones.filter(
+            (quote) => quote.name == dolar.casa.nombre
+          );
+          console.log(descripcionDollar);
+          values.nombre = dolar.casa.nombre;
+          values.compra = dolar.casa.compra.replace(",", ".");
+          values.venta = dolar.casa.venta.replace(",", ".");
+          values.variacion = dolar.casa.variacion?.replace(",", ".");
+          values.descripcion = !descripcionDollar.length
+            ? ""
+            : descripcionDollar[0].descripcion;
+          return values;
+        });
+        setDollarCalculator(dollarValues);
+        setLoadingData(false);
+      } catch (e) {
+        setLoadingData(false);
+        console.error(e);
+      }
     }
     getDollar();
   }, []);
@@ -53,12 +61,35 @@ export default function DollarCalculator() {
         <title>Dólar - ¿Que me están cobrando?</title>
         <meta></meta>
       </Head>
-      <main className={`${inter.className} relative flex justify-center pt-20`}>
+      <main
+        className={`${inter.className} relative mb-10 flex flex-col justify-center pt-20`}
+      >
         <CalculatorContainer
           dollarCalculator={dollarCalculator ?? dollarCalculator}
           lastUpdate={lastUpdate}
+          loadingData={loadingData}
         />
       </main>
+      <footer className="h-auto w-full border-t border-[#444] bg-[#222] px-2 py-10 sm:px-8">
+        <p className="text-center text-sm text-white">
+          Todas las cotizaciones, indices y cualquier otro valor publicadas en
+          este sitio web son a fines informativos y tienen un carácter
+          orientativo, por lo que deben ser tomados únicamente a modo de
+          referencia.
+          <br />
+          <br />
+          El titular de este sitio web no puede bajo ningún punto de vista
+          garantizar la veracidad y/o exactitud y/o vigencia de los datos
+          mostrados en el mismo.
+          <br />
+          <br />
+          El titular de este sitio web deslinda toda responsabilidad por los
+          daños y/o perjuicios que pudiere ocasionar la toma de de decisiones en
+          base a la información recabada en el mismo, como así tampoco se hace
+          responsable sobre el uso que puedan hacer terceros con la información
+          brindada.
+        </p>
+      </footer>
     </>
   );
 }
