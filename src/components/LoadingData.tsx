70f20849705421c49a863eb565f23afc57f1f648
Matias Seitour <matias.seitour01@gmail.com>
2023-07-28 13:16:52 -0300

    feat(loadingData) add loading spinner

---
+++ src/components/LoadingData.tsx
@@ -0,0 +1,10 @@
+export const LoadingData = (): JSX.Element => {
+  return (
+    <div className="lds-ring">
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
