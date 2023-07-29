0c77d1e04d05dd537c5e075497e24ac62c4570f1
Matias Seitour <matias.seitour01@gmail.com>
2023-07-29 20:16:22 -0300

    feat(dolar-calculator) add "¿que me estan cobrando" link

--- src/pages/dolar-calculator.tsx
+++ src/pages/dolar-calculator.tsx
@@ -4,6 +4,8 @@ import { inter } from "@/utils/fonts";
 import axios from "axios";
 import { CalculatorContainer } from "@/components/CalculatorContainer";
 import quotesData from "../assets/quotes.json";
+import { Footer } from "@/components/Footer";
+import Link from "next/link";
 
 export default function DollarCalculator() {
   const { cotizaciones } = quotesData;
@@ -69,27 +71,56 @@ export default function DollarCalculator() {
           lastUpdate={lastUpdate}
           loadingData={loadingData}
         />
+        <div className="flex h-auto w-full flex-col justify-center gap-4 pt-10">
+          <div className="flex flex-col items-center justify-center px-4">
+            <p className="home-title bg-clip-text text-center text-2xl font-bold">
+              ¿Confundido/a de no saber que te cobran por tu servicio digital
+              favorito?
+            </p>
+            <br />
+            <p className="home-title bg-clip-text text-center text-2xl font-bold">
+              ¿Querés comprarte ese juego que tanto querés y no sabes el precio{" "}
+              final de ese juego?
+            </p>{" "}
+          </div>
+          <div className="flex w-full flex-col items-center justify-center gap-4 sm:flex-row">
+            <span className="home-title bg-clip-text text-2xl font-bold">
+              Visitá
+            </span>
+            <Link
+              href={"/"}
+              className="flex items-center justify-center rounded-md bg-gradient-to-r from-emerald-500 to-cyan-500 p-2 text-2xl font-semibold text-transparent text-white"
+              type="button"
+            >
+              <p className="">¿Que me están cobrando?</p>
+            </Link>
+          </div>
+        </div>
       </main>
-      <footer className="h-auto w-full border-t border-[#444] bg-[#222] px-2 py-10 sm:px-8">
-        <p className="text-center text-sm text-white">
-          Todas las cotizaciones, indices y cualquier otro valor publicadas en
+      <Footer
+        text=" Todas las cotizaciones, indices y cualquier otro valor publicadas en
           este sitio web son a fines informativos y tienen un carácter
           orientativo, por lo que deben ser tomados únicamente a modo de
-          referencia.
-          <br />
-          <br />
-          El titular de este sitio web no puede bajo ningún punto de vista
-          garantizar la veracidad y/o exactitud y/o vigencia de los datos
-          mostrados en el mismo.
-          <br />
-          <br />
-          El titular de este sitio web deslinda toda responsabilidad por los
-          daños y/o perjuicios que pudiere ocasionar la toma de de decisiones en
-          base a la información recabada en el mismo, como así tampoco se hace
-          responsable sobre el uso que puedan hacer terceros con la información
-          brindada.
-        </p>
-      </footer>
+          referencia. El titular de este sitio web no puede bajo ningún punto de
+          vista garantizar la veracidad y/o exactitud y/o vigencia de los datos
+          mostrados en el mismo. El titular de este sitio web deslinda toda
+          responsabilidad por los daños y/o perjuicios que pudiere ocasionar la
+          toma de de decisiones en base a la información recabada en el mismo,
+          como así tampoco se hace responsable sobre el uso que puedan hacer
+          terceros con la información brindada."
+      />
     </>
   );
+}
+
+{
+  /* <p>
+              ¿Confundido/a de no saber que te cobran por tu servicio digital
+              favorito?
+            </p>
+            <br />
+            <p>
+              ¿Querés comprarte ese juego que tanto querés y no sabes el precio{" "}
+              final de ese juego?
+            </p> */
 }
