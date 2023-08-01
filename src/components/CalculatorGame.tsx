75fc2a22f12090b6484774df72558e590ac0f53b
Matias Seitour <matias.seitour01@gmail.com>
2023-07-31 22:19:29 -0300

    feat(calculatorGame) add new types and comments

--- src/components/CalculatorGame.tsx
+++ src/components/CalculatorGame.tsx
@@ -1,16 +1,23 @@
-import { useState, ChangeEvent } from "react";
+import { useState } from "react";
 import { GamePesos } from "./GamePesos";
 import { GameUsd } from "./GameUsd";
 
+// Enum with 2 types of values: dollar or pesos
 const enum Badge {
   USD = "USD",
   PESOS = "pesos",
 }
 
-export const CalculatorGame = ({ dollar }: { dollar: any }): JSX.Element => {
+export const CalculatorGame = ({
+  dollar,
+}: {
+  dollar: { dollarValue: string; lastUpdate: string };
+}): JSX.Element => {
+  // Creates a state whose initial value is pesos
   const [badge, setBadge] = useState<Badge>(Badge.PESOS);
   const [copyLastPriceState, setCopyLastPriceState] = useState<boolean>(false);
 
+  // Function that makes the "message copied" message appear and disappear
   const toggleCopy = () => {
     setCopyLastPriceState(true);
     setTimeout(() => {
