94ce02e6d54fa238068a309b07b2d52a68ea97f3
Matias Seitour <matias.seitour01@gmail.com>
2023-07-31 22:16:56 -0300

    feat(dolar-calculator) import types and add comments

--- src/pages/dolar-calculator.tsx
+++ src/pages/dolar-calculator.tsx
@@ -6,56 +6,69 @@ import { CalculatorContainer } from "@/components/CalculatorContainer";
 import quotesData from "../assets/quotes.json";
 import { Footer } from "@/components/Footer";
 import { LinkToHome } from "@/components/LinkToHome";
+import { DollarApi, DollarNewValues } from "@/types";
 
 export default function DollarCalculator() {
+  // Get "cotizaciones" from quotes.json
   const { cotizaciones } = quotesData;
 
-  const [dollarCalculator, setDollarCalculator] = useState<any>();
+  // create 2 states: one for the values that will be returned by the api call and the other for the state of loading
+  const [dollarCalculator, setDollarCalculator] = useState<
+    DollarNewValues[] | undefined
+  >();
   const [loadingData, setLoadingData] = useState<boolean>(false);
+
   useEffect(() => {
+    //call to the dolarsi api
     async function getDollar() {
       try {
         setLoadingData(true);
         const res = await axios.get(
           "https://www.dolarsi.com/api/api.php?type=valoresprincipales"
         );
-        const filterRes = res.data.filter(
-          (value: any) => {
-            if (value.casa.nombre == "Bitcoin") {
-              return;
-            }
-            if (value.casa.nombre == "Argentina") {
-              return;
-            }
-            if (value.casa.nombre == "Dolar") {
-              return;
-            }
-            return value.casa;
-          }
-          // value.casa.nombre != "Argentina"
-        );
-        console.log(filterRes);
-        const dollarValues = filterRes.map((dolar: any) => {
-          const values = {
-            nombre: "",
-            compra: null,
-            venta: null,
-            variacion: "",
-            descripcion: "",
-          };
-          const descripcionDollar = cotizaciones.filter(
-            (quote) => quote.name == dolar.casa.nombre
-          );
 
-          values.nombre = dolar.casa.nombre;
-          values.compra = dolar.casa.compra.replace(",", ".");
-          values.venta = dolar.casa.venta.replace(",", ".");
-          values.variacion = dolar.casa.variacion?.replace(",", ".");
-          values.descripcion = !descripcionDollar.length
-            ? ""
-            : descripcionDollar[0].descripcion;
-          return values;
+        // Dolarsi api returns some values that are obsolete, check that if it is one of those values, it omits it
+        const filterRes = res.data.filter((value: any) => {
+          if (value.casa.nombre == "Bitcoin") {
+            return;
+          }
+          if (value.casa.nombre == "Argentina") {
+            return;
+          }
+          if (value.casa.nombre == "Dolar") {
+            return;
+          }
+          return value.casa;
         });
+
+        // Create a new array with the values I want to use
+        const dollarValues: DollarNewValues[] = filterRes.map(
+          (dolar: DollarApi) => {
+            const values = {
+              nombre: "",
+              compra: "",
+              venta: "",
+              variacion: "",
+              descripcion: "",
+            };
+
+            // Creates an array that filters if the current dollar value name is equal to the quotes name coming from quotes.json
+            const descripcionDollar = cotizaciones.filter(
+              (quote) => quote.name == dolar.casa.nombre
+            );
+
+            // Set the dollar values by replacing the "," with "."
+            // Why do i do this? because in other files I will want to parse a value using Number(value). if I don't change "," to ".", it would return NaN
+            values.nombre = dolar.casa.nombre;
+            values.compra = dolar.casa.compra.replace(",", ".");
+            values.venta = dolar.casa.venta.replace(",", ".");
+            values.variacion = dolar.casa.variacion?.replace(",", ".");
+            values.descripcion = !descripcionDollar.length
+              ? ""
+              : descripcionDollar[0].descripcion;
+            return values;
+          }
+        );
         setDollarCalculator(dollarValues);
         setLoadingData(false);
       } catch (e) {
@@ -66,6 +79,7 @@ export default function DollarCalculator() {
     getDollar();
   }, []);
 
+  // Create a date format of the last update
   const time = new Date();
   const lastUpdate = `${time.getDay().toString()}/${
     time.getMonth() + 1
@@ -107,16 +121,4 @@ export default function DollarCalculator() {
       />
     </>
   );
-}
-
-{
-  /* <p>
-              ¿Confundido/a de no saber que te cobran por tu servicio digital
-              favorito?
-            </p>
-            <br />
-            <p>
-              ¿Querés comprarte ese juego que tanto querés y no sabes el precio{" "}
-              final de ese juego?
-            </p> */
 }
