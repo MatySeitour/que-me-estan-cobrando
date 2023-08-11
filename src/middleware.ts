4416b13ec596a78fdf5ab103911d1ef44af1e4a2
Matias Seitour <matias.seitour01@gmail.com>
2023-08-11 13:32:18 -0300

    fix(middleware) fix file name

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
+
+export function middleware(request: NextRequest, respose: NextResponse) {
+  const { searchParams } = new URL(request.url);
+  const key = searchParams.get("key");
+
+  if (process.env.API_KEY_SCRAPING !== key) {
+    return new NextResponse(
+      JSON.stringify({ success: false, message: "authentication failed" }),
+      { status: 401, headers: { "content-type": "application/json" } }
+    );
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: "/api/cron/:path*",
+};
