f19b7d5bc722c6b9bb0287be0cc4da35d3760ef0
Matias Seitour <matias.seitour01@gmail.com>
2023-08-11 16:15:09 -0300

    test

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,13 +4,14 @@ import type { NextRequest } from "next/server";
 export function middleware(request: NextRequest, respose: NextResponse) {
   const { searchParams } = new URL(request.url);
   const key = searchParams.get("key");
+  console.log();
 
-  if (process.env.API_KEY_SCRAPING !== key) {
-    return new NextResponse(
-      JSON.stringify({ success: false, message: "authentication failed" }),
-      { status: 401, headers: { "content-type": "application/json" } }
-    );
-  }
+  // if (process.env.API_KEY_SCRAPING !== key) {
+  //   return new NextResponse(
+  //     JSON.stringify({ success: false, message: "authentication failed" }),
+  //     { status: 401, headers: { "content-type": "application/json" } }
+  //   );
+  // }
 
   return NextResponse.next();
 }
