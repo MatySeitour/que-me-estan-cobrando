0c97235b6ccf7a0dff5ac73e4d49c007c9e3d93f
Matias Seitour <matias.seitour01@gmail.com>
2023-08-16 22:38:20 -0300

    feat(clientPage) add new service type

--- src/components/ClientPage.tsx
+++ src/components/ClientPage.tsx
@@ -4,43 +4,29 @@ import TitleServices from "@/components/TitleServices";
 import { Service } from "@/components/Service";
 import { Games } from "@/components/Games";
 import { ServicesContainer } from "@/containers/ServicesContainer";
-import { useState, useEffect } from "react";
-import type { ServiceType } from "@/types";
-import plataformasData from "../assets/plataformas.json";
-import servicesApi from "@/utils/getServices";
+import { useState } from "react";
+import type { ServiceTest } from "@/types";
 
-export const ClientPage = (): JSX.Element => {
-  // extrae los datos de las plataformas del plataformas.json
-  const { plataformas } = plataformasData;
-
+export const ClientPage = ({
+  services,
+}: {
+  services: ServiceTest[];
+}): JSX.Element => {
   // Por default, el estado inicial va a ser el primer elemento de las plataformas(en este caso, netflix).
-  const [serviceSelected, setServiceSelected] = useState<ServiceType>(
-    plataformas[0]
+  const [serviceSelected, setServiceSelected] = useState<ServiceTest>(
+    services[0]
   );
-
-  useEffect(() => {
-    servicesApi.getServices().then((services) => console.log(services));
-  }, []);
 
   return (
     <div className="mx-auto max-w-7xl">
-      <Banner />
+      <Banner services={services} />
       <div id="service-container" className="mx-auto h-auto w-full px-4 pt-28">
         <ServicesContainer>
+          <TitleServices />
           <div id="select-service__container">
-            <TitleServices />
             <SelectService
-              serviceSelected={
-                serviceSelected ?? {
-                  id: -1,
-                  imagen: "",
-                  planes: [],
-                  nombre: "",
-                  impuesto_final: 0,
-                  impuesto_porcentaje: 0,
-                  select_price: false,
-                }
-              }
+              services={services}
+              serviceSelected={serviceSelected}
               setServiceSelected={setServiceSelected}
             />
           </div>
