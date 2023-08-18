6dc2f77906c579adf65e5ccbbf11a2c6602ae9c5
Matias Seitour <matias.seitour01@gmail.com>
2023-08-17 23:12:42 -0300

    feat(titleServices) add new animation

--- src/components/TitleServices.tsx
+++ src/components/TitleServices.tsx
@@ -4,7 +4,7 @@ import { GradientEffectBackground } from "./GradientEffectBackground";
 export default function TitleServices() {
   return (
     <>
-      <h2 className="bg-gradient__effect service-title relative mb-8 text-center text-[3rem]">
+      <h2 className="bg-gradient__effect services-title relative mb-8 text-center text-[3rem]">
         Plataformas{" "}
         <b className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal">
           Digitales
@@ -12,7 +12,7 @@ export default function TitleServices() {
         <GradientEffectBackground classname="after:z-50 before:z-50 z-50 top-0" />
       </h2>
       <p
-        className={`text-center text-white/30 md:text-lg lg:text-xl ${inter.className} home-title`}
+        className={`text-center text-white/30 md:text-lg lg:text-xl ${inter.className} home-title services-select__subtitle`}
       >
         Selecciona el servicio que quieras para ver los{" "}
         <b className="bg-gradient-to-r from-emerald-500 to-cyan-500 bg-clip-text font-normal">
