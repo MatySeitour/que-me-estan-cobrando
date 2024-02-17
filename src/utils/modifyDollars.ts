2c6b52027b295f0dd879d8430e6aaf7ba5260699
Matias Seitour <matias.seitour01@gmail.com>
2024-02-17 14:30:31 -0300

    fix(modifyDollars) change logic to get dollars prices

--- src/utils/modifyDollars.ts
+++ src/utils/modifyDollars.ts
@@ -3,46 +3,22 @@ import quotesData from "../assets/quotes.json";
 
 export default function modifyDollars(dollars: DollarApi[]) {
   const { cotizaciones } = quotesData;
-
-  const filterRes = dollars.filter((value: any) => {
-    if (value.casa.nombre == "Bitcoin") {
-      return;
-    }
-    if (value.casa.nombre == "Argentina") {
-      return;
-    }
-    if (value.casa.nombre == "Dolar") {
-      return;
-    }
-    return value.casa;
-  });
-
   // Create a new array with the values I want to use
-  const dollarValues: any = filterRes.map((dolar: DollarApi) => {
-    const values = {
-      nombre: "",
-      compra: "",
-      venta: "",
-      variacion: "",
-      descripcion: "",
-      ultimActualizacion: "",
-    };
-
-    // Creates an array that filters if the current dollar value name is equal to the quotes name coming from quotes.json
+  const dollarValues: any = dollars.map((dolar: any) => {
     const descripcionDollar = cotizaciones.filter(
-      (quote) => quote.name == dolar.casa.nombre
+      (quote) => quote.name == dolar.nombre
     );
-
-    // Set the dollar values by replacing the "," with "."
-    // Why do i do this? because in other files I will want to parse a value using Number(value). if I don't change "," to ".", it would return NaN
-    values.nombre = dolar.casa.nombre;
-    values.compra = dolar.casa.compra.replace(",", ".");
-    values.venta = dolar.casa.venta.replace(",", ".");
-    values.variacion = dolar.casa.variacion?.replace(",", ".");
-    values.descripcion = !descripcionDollar.length
-      ? ""
-      : descripcionDollar[0].descripcion;
-    return values;
+    return {
+      nombre: dolar.nombre,
+      compra: dolar.compra,
+      venta: dolar.venta,
+      variacion: "",
+      descripcion: !descripcionDollar.length
+        ? ""
+        : descripcionDollar[0].descripcion,
+      ultimActualizacion: dolar.fechaActualizacion,
+    };
   });
+
   return dollarValues;
 }
