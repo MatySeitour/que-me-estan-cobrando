7f6ed35398fc31e687de0c84df905bd0314a0fc7
Matias Seitour <matias.seitour01@gmail.com>
2024-02-17 14:34:22 -0300

    fix(clientDollarPage) remove dynamic import

--- src/components/ClientDollarPage.tsx
+++ src/components/ClientDollarPage.tsx
@@ -1,23 +1,17 @@
 import { DollarApi } from "@/types";
 import modifyDollars from "@/utils/modifyDollars";
-import dynamic from "next/dynamic";
+import { CalculatorContainer } from "../components/CalculatorContainer";
 
-const Calculator = dynamic(() =>
-  import("../components/CalculatorContainer").then(
-    (module) => module.CalculatorContainer
-  )
-);
-
-export const ClientDollarPage = ({
+export default function ClientDollarPage({
   dollars,
 }: {
   dollars: DollarApi[];
-}): JSX.Element => {
+}) {
   const newDollars = modifyDollars(dollars);
 
   return (
     <section>
-      <Calculator dollarCalculator={newDollars} />
+      <CalculatorContainer dollarCalculator={newDollars} />
     </section>
   );
-};
+}
